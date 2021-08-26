e36f0d395486cf6a98a8013f7bae86cb89d5746b
JCFercik <jc.fercikferreira@gmail.com>
2021-08-25 21:27:48 -0300

    Implementando o desafio de KM para minutos

---
+++ Primeiros passos com C#/Desafio - Distância/Desafio/Desafio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a distância entre Y e X em KM ");
+            int quilometros = Int32.Parse(Console.ReadLine());
+            int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
+            Console.WriteLine(minutos + " minutos");
+        }
+    }
+}
