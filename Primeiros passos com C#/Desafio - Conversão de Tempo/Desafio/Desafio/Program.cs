812f610cc671aafc801c92a4566a5fa003c94bb6
JCFercik <jc.fercikferreira@gmail.com>
2021-08-27 22:08:41 -0300

    Desafio DDD

---
+++ Primeiros passos com C#/Desafio - Conversão de Tempo/Desafio/Desafio/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Desafio
+    Você terá o desafio de ler um valor inteiro, que é o tempo de duração em segundos de um 
+    determinado evento em uma loja, e informe-o expresso no formato horas:minutos:segundos.
+ */
+
+using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeInSeconds = int.Parse(Console.ReadLine());
+            //timeInSeconds =  ;
+            //continue a solucao
+            var hours = timeInSeconds/3600;
+            var minutes = timeInSeconds/60;
+            var seconds = timeInSeconds-((hours+minutes)*60);
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
+}
